c3882e368d844f167b04df62417672b158d7b2d3
Lukas Krähling <lukas.K98@web.de>
2020-05-29 16:46:15 +0200

    Import T813XXS2BRF2 kernel source changes.

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,7 +5,6 @@ export PATH=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-and
 export SEC_BUILD_OPTION_HW_REVISION=02
 
 mkdir out
-mkdir out
 
 make -C $(pwd) O=$(pwd)/out ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- KCFLAGS=-mno-android VARIANT_DEFCONFIG=msm8976_sec_gts28vewifi_eur_defconfig msm8976_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig SELINUX_LOG_DEFCONFIG=selinux_log_defconfig TIMA_DEFCONFIG=tima8976_defconfig DMVERITY_DEFCONFIG=dmverity_defconfig
 make -C $(pwd) O=$(pwd)/out ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- KCFLAGS=-mno-android
